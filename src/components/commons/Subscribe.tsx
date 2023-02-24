57bfe428181c90f777ec16b067cd44c8e63c400c
Mohsen Farzadipour <onemohsen@gmail.com>
2023-02-25 01:13:56 +0330

    feat: implement subscribe component

---
+++ src/components/commons/Subscribe.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import PrimaryButton from "./button/PrimaryButton";
+import TextInput from "./input/TextInput";
+
+const Subscribe = () => {
+  return (
+    <div className="text-center flex flex-col space-y-3">
+      <span className="font-bold">Subscribe on our newsletter</span>
+      <span>
+        Get daily news on upcoming offers from many suppliers all over the world
+      </span>
+      <div>
+        <TextInput name="test" id="test" placeholder="Email" />
+        <PrimaryButton title="Subscribe" />
+      </div>
+    </div>
+  );
+};
+
+export default Subscribe;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,16 @@
+import Subscribe from "@/components/commons/Subscribe";
+import ContainerWrapper from "@/components/commons/wrapper/ContainerWrapper";
 import { Inter } from "@next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <div className="py-20">Home Page</div>;
+  return (
+    <div>
+      <ContainerWrapper className="py-20">Home Page</ContainerWrapper>
+      <div className="bg-gray-200 py-10">
+        <Subscribe />
+      </div>
+    </div>
+  );
 }
